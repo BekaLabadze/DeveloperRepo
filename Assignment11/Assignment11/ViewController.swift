0c1b4c04ab3d1552862b91ecf2f1c1eb319b0ebb
BekaL <beka75610@gmail.com>
2024-10-08 18:14:47 +0400

    Changes were made after review

--- Assignment11/Assignment11/ViewController.swift
+++ Assignment11/Assignment11/ViewController.swift
@@ -8,16 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var numberOne: UITextField!
-    @IBOutlet weak var numberTwo: UITextField!
-    @IBOutlet weak var switcher: UISwitch!
-    @IBOutlet weak var result: UILabel!
-    @IBOutlet weak var switcherStatus: UILabel!
+    @IBOutlet private weak var numberOne: UITextField!
+    @IBOutlet private weak var numberTwo: UITextField!
+    @IBOutlet private weak var switcher: UISwitch!
+    @IBOutlet private weak var result: UILabel!
+    @IBOutlet private weak var switcherStatus: UILabel!
+    @IBOutlet private weak var switchAppearance: UISwitch!
+    @IBOutlet private weak var appearance: UILabel!
     
-    @IBOutlet weak var switchAppearance: UISwitch!
+    @IBAction private func calculate(_ sender: UIButton) {
+        calculation()
+    }
+    @IBAction private func switchAppearance(_ sender: UISwitch) {
+        changeInterface()
+    }
+    @IBAction private func switcherAction(_ sender: UISwitch) {
+        divideByZero()
+    }
     
-    @IBOutlet weak var appearance: UILabel!
-    @IBAction func calculate(_ sender: UIButton) {
+    private func calculation() {
         if let textOne = numberOne.text, let textTwo = numberTwo.text, let firstNum = Int(textOne), let secondNum = Int(textTwo) {
             if secondNum == 0 {
                 result.text = ("0-ზე გაყოფა არ შეიძლება")
@@ -31,7 +40,7 @@ class ViewController: UIViewController {
         }
     }
     
-    @IBAction func switcherAction(_ sender: UISwitch) {
+    private func divideByZero() {
         if switcher.isOn {
             switcherStatus.text = "უნაშთო გაყოფა"
         } else {
@@ -39,7 +48,7 @@ class ViewController: UIViewController {
         }
     }
     
-    @IBAction func switchAppearance(_ sender: UISwitch) {
+    private func changeInterface() {
         if switchAppearance.isOn {
             appearance.text = "Appearance: Light Mode"
             overrideUserInterfaceStyle = .light
