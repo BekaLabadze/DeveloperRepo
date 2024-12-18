74cc7bda0c2d2fb472a94e9fffc8982091db2a6f
BekaL <beka75610@gmail.com>
2024-12-18 20:00:29 +0400

    Ready for review

---
+++ HomeAssignment33/HomeAssignment33/HomeAssignment33App.swift
@@ -0,0 +1,17 @@
+//
+//  HomeAssignment33App.swift
+//  HomeAssignment33
+//
+//  Created by Beka on 18.12.24.
+//
+
+import SwiftUI
+
+@main
+struct HomeAssignment33App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HomeAssignment33/HomeAssignment33/TrashView.swift
@@ -0,0 +1,18 @@
+//
+//  TrashView.swift
+//  HomeAssignment33
+//
+//  Created by Beka on 18.12.24.
+//
+
+
+import SwiftUI
+
+struct TrashView: View {
+    var body: some View {
+        ZStack {
+            Color.green.edgesIgnoringSafeArea(.top)
+            Image(.folder)
+        }
+    }
+}
